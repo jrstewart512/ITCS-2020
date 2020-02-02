bdf895ddb73f0eb05505515f3735a0e9e0df3299
jrstewart512 <jrsewart512@gmail.com>
2020-02-02 21:16:01 +0000

    Edge cases

--- rmsim/rmsim.py
+++ rmsim/rmsim.py
@@ -24,6 +24,10 @@ def execute(lineNo):
             decjz(line[1], line[2])
         elif line[0] == 'inc':
             inc(line[1])
+        # Return error on unknown instruction
+        else:
+            print("Error: Invalid instruction on line " + lineNo)
+            sys.exit(0)
 
         return execute(lineNo + 1)
 
@@ -33,7 +37,7 @@ def inc(register):
     # Retrieve register index
     regIndex = int(register.split('r')[1])
     # If register has not been created yet, initialise it to zero (along with any intermediate registers)
-    while regIndex > len(registers) - 1:
+    while regIndex > len(list(registers)) - 1:
         registers.append(0)
     # Increment register value
     registers[regIndex] += 1
@@ -52,8 +56,12 @@ def decjz(register, label):
         if label == 'HALT':
             execute(-1)
         else:
-            lineNo = labels.get(label)
-            execute(lineNo)
+            if label in labels:
+                lineNo = labels.get(label)
+                execute(lineNo)
+            else:
+                print("Error: Undefined label")
+                sys.exit(0)
     else:
         registers[regIndex] -= 1
         return
@@ -62,16 +70,31 @@ def decjz(register, label):
 def parseLines():
     global program
     global registers
+
+    # Ignore commented (#) lines
+    for i in range(len(program)):
+        if program[i][0] == '#':
+            del program[i]
+
     # Retrieve initial register values
-    registers.append(map(int, program[0][1:len(program[0])]))
-    registers = registers[0]
+    registers = list(map(int, program[0][1:len(program[0])]))
+
     # Remove registers line from program
     program = program[1:]
+    # If program is empty, print register values and exit
+    if not program:
+        execute(-1)
 
-    # Retrieve labels and add them to dict
+    # Various program alterations
     for i in range(len(program)):
+        # Retrieve labels and add to dictionary
         if program[i][0][-1] == ':':
-            labels[program[i][0][:-1]] = i
+            # Check for duplicate labels
+            if program[i][0] in labels:
+                print("Error: Duplicate label on line " + i)
+                sys.exit(0)
+            else:
+                labels[program[i][0][:-1]] = i
 
 
 def main():
